ff1ff8002d4e7249c58098fc757aa9a4889d10f8
Ashita Arneja <anejaashita@gmail.com>
2020-10-20 17:50:02 +0530

    Added lasso model with test cases

---
+++ datascientist/model/regression/skl/linear_model/lasso.py
@@ -0,0 +1,33 @@
+from datascientist.model.regression.evaluation_metrics.mse import _mse
+from datascientist.model.regression.evaluation_metrics.mae import _mae
+from datascientist.model.regression.evaluation_metrics.rmse import _rmse
+
+from sklearn.linear_model import Lasso
+import numpy as np
+
+
+def _lasso(*, train, test, x_predict=None, metrics, alpha=1.0, fit_intercept=True, normalize=False,
+    precompute=False, copy_X=True, max_iter=1000, tol=0.0001, warm_start=False, positive=False,
+     random_state=None, selection='cyclic'):
+    """For for info visit : 
+        https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.Lasso.html#sklearn.linear_model.Lasso
+    """
+    model = Lasso(alpha=alpha, fit_intercept=fit_intercept, normalize=normalize, precompute=precompute, copy_X=copy_X,
+        max_iter=max_iter, tol=tol, warm_start=warm_start, positive=positive, random_state=random_state, selection=selection)
+    model.fit(train[0], train[1])
+    model_name = 'Lasso'
+    y_hat = model.predict(test[0])
+
+    if metrics == 'mse':
+        accuracy = _mse(test[1], y_hat)
+    if metrics == 'rmse':
+        accuracy = _rmse(test[1], y_hat)
+    if metrics == 'mae':
+        accuracy = _mae(test[1], y_hat)
+
+    if x_predict is None:
+        return (model_name, accuracy, None)
+
+    y_predict = model.predict(x_predict)
+    return (model_name, accuracy, y_predict)
+    
