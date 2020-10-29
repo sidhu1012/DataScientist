c7de25bd154e7f4796f381af2036bbbc5564f4d4
Ashita Arneja <anejaashita@gmail.com>
2020-10-29 17:51:18 +0530

    Corrected intendation

--- datascientist/model/regression/skl/linear_model/tests/test_lassocv.py
+++ datascientist/model/regression/skl/linear_model/tests/test_lassocv.py
@@ -34,6 +34,6 @@ def test_lassocv():
     for i in range(len(answer[2])):
         assert round(answer[2][i], 2) == round(arr[i], 2)
 
-x_train = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
+    x_train = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
     y_train = np.dot(x_train, np.array([1, 2])) + 3
     raises(ValueError, lambda: _lassocv(train=(x_train, y_train), test=(x_test, y_test), metrics=metrics, x_predict=x_test))
