4d6e6faae2d16e14752348863f5db787b0f8add3
SHREYAS PJ <shreyaspj20@gmail.com>
2020-12-01 23:43:53 +0530

    gradient_boosting_regression + test cases modified

--- datascientist/model/regression/skl/ensemble_model/gradient_boosting_regression.py
+++ datascientist/model/regression/skl/ensemble_model/gradient_boosting_regression.py
@@ -10,7 +10,7 @@ def _gradient_boosting_regression(*, train, test, x_predict=None, metrics,
                                   loss='ls', learning_rate=0.1, n_estimators=100, subsample=1.0,
                                   min_samples_split=2, min_samples_leaf=1,
                                   min_weight_fraction_leaf=0.0, max_depth=3, min_impurity_decrease=0.0,
-                                  min_impurity_split=None, init=None, random_state=76, max_features=None,
+                                  min_impurity_split=None, init=None, random_state=None, max_features=None,
                                   alpha=0.9, verbose=0, max_leaf_nodes=None, warm_start=False, presort='deprecated',
                                   validation_fraction=0.1, n_iter_no_change=None, tol=0.0001, ccp_alpha=0.0):
     """For for info visit :
