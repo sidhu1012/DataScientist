e35ce5bad345e986769657dda38d7763001f3a7a
Parva-Jain <parvajainpjjp@gmail.com>
2020-12-03 15:28:20 +0530

    Added documentation and folder for csv file

--- datascientist/feature_selection/filter_based_selection.py
+++ datascientist/feature_selection/filter_based_selection.py
@@ -4,45 +4,62 @@ Created on Wed Dec  2 14:28:56 2020
 
 @author: parva
 """
-
-
 import pandas as pd
 import numpy as np
 
-'''Note:- Only numerical or categorcial values encoded to numerical can be passed as features or target. Anything else data type will throw error if passed. '''
-
 class PearsonCorrelation:
+    '''
+    A feature Selection method using Pearson Correlation.
     
-    '''https://en.wikipedia.org/wiki/Pearson_correlation_coefficient'''
+    A Pearson Correlation Coefficient is calculated using a feature and target
+    of a dataset and is calculated for each feature. Coefficient can take value
+    between 1 and -1. If the value is near 1 then that feature is positively 
+    correlated(directly proportional) to the target, if the value is near -1 
+    then that feature is negatively correlated(inversly proportinal) to the
+    target and if the value is near 0 then that feature is not related to the
+    target.
     
-    '''Params:
-        features:Features are individual independent variables that act as the input in your system.
-        Prediction models use features to make predictions.
+    Parameters:
+    ----------
+    features: DataFrame
+        Features are individual independent variables that act as the input in 
+        your system. The features DataFrame should only contain numerical value
+        (if categorical value present then encode them to numerical labels).
         
-        target : The target is whatever the output of the input variables.
-        It could be the individual classes that the input variables maybe mapped to in case of a classification problem or the output value range in a regression problem.     
-        '''
+    target: Series
+        The target is whatever the output of the input variables.It could be 
+        the individual classes that the input variables maybe mapped to in case
+        of a classification problem or the output value range in a regression 
+        problem. The target series should also contain numerical value like 
+        features DataFrame.
+    '''
     
     def __init__(self,features,target):
         
         self.X = features
         self.y = target
-        
-    
     
     def corr_score(self,sort = False,reset_index = False):
-        
-        '''Evaluate the Pearson Correlation Coefficient of each feature column with the target column.
+        '''
+        Evaluate the Pearson Correlation Coefficient of each feature column 
+        with the target column.
         
         Parameters
         ----------
-        - sort: boolean value(default = False),if set True, then arranges the features in descending order of coefficients value.
-        - reset_index: boolean value(default = False),if set True, then resets the index of output dataframe.
+        sort: bool, default=False
+            Whether to sort the order of features according to the coefficient
+            value. If True, the return DataFrame is sorted in descending order
+            according to correlation coefficients value.
+            
+        reset_index: bool, default=False
+            Whether to reset the index of the return DataFrame. If True, then 
+            resets the index of output dataframe.
         
         Returns
         -------
-        -cor_score: DataFrame with one column as the features name and the other as Coefficient value.'''
-                       
+        cor_score: DataFrame with one column as the features name and the other
+        as Coefficient value.
+        '''
         cor_list = []
         feature_name = self.X.columns.tolist()
         cor_score = pd.DataFrame(columns = ['feature','cor_score'])
@@ -53,24 +70,31 @@ class PearsonCorrelation:
         cor_score['feature'] = feature_name
         cor_score['cor_score'] = cor_list
         if sort == True:
-            cor_score = cor_score.sort_values(by = ['cor_score'],ascending = False)
+            cor_score = cor_score.sort_values(by = ['cor_score']
+                                              ,ascending = False)
         if reset_index == True:
             cor_score = cor_score.reset_index(drop = True)
         
         return cor_score
-    
-    
-    def top_corr_featurenames(self,feat_num = 1,ascending = True):
         
-        '''Parameters
+    def top_corr_featurenames(self,feat_num = 1,ascending = True):
+        '''
+        Parameters
         ----------
-        - feat_num: integer value(default = 1),can take value from 1 to total no. of features, to decide the top feat_num of features according to their coefficient value. 
-        - ascending: boolean value(default = True),if set False, then the feature with higher coefficient value is first element folloewed by lower ones. 
+        feat_num: int, default=1
+            The number of top features names to return according to the 
+            correlation coefficient value.
+            
+        ascending: bool, default=True
+            Whether order of return list is in ascending order according to the
+            correlation coefficient value. If False, then the feature with 
+            higher coefficient value is first element followed by the elements 
+            with lower values. 
         
         Returns
         -------
-        -cor_feature: List containing top feat_num features name. '''
-        
+        cor_feature: List containing top feat_num features name.
+        '''
         cor_list = []
         feature_name = self.X.columns.tolist()
         # cor_score = pd.DataFrame(columns = ['feature','cor_score'])
@@ -82,19 +106,26 @@ class PearsonCorrelation:
         if ascending == False:
             cor_feature = cor_feature[::-1]
         return cor_feature
-    
-    
-    def top_corr_features(self,feat_num = 1,ascending = True):
         
-        '''Parameters
+    def top_corr_features(self,feat_num = 1,ascending = True):
+        '''
+        Parameters
         ----------
-        - feat_num: integer value(default = 1),can take value from 1 to total no. of features, to decide the top feat_num of features according to their coefficient value. 
-        - ascending: boolean value(default = True),if set False, then the feature with higher coefficient value is first column folloewed by lower ones. 
+        feat_num: int, default=1
+            The number of top features(with all the data) to return according 
+            to the correlation coefficient value.
+            
+        ascending: bool, default=True
+            Whether the columns of return DataFrame is in ascending order
+            according to the correlation coefficient value. If False, then the
+            feature with higher coefficient value is first column followed by 
+            the features with lower values. 
         
         Returns
         -------
-        -out_feature: DataFrame containing top feat_num features with all the data(rows). '''
-        
+        out_feature: DataFrame containing top feat_num features with all the 
+        data(rows).
+        '''
         cor_list = []
         feature_name = self.X.columns.tolist()
         # cor_score = pd.DataFrame(columns = ['feature','cor_score'])
@@ -105,7 +136,4 @@ class PearsonCorrelation:
         if ascending == False:
             cor_feature = cor_feature[::-1]
         out_features = self.X[cor_feature]
-        return out_features
-    
-    
-    
+        return out_features       
--- datascientist/feature_selection/test/Pearson_test.py
+++ datascientist/feature_selection/test/Pearson_test.py
@@ -5,27 +5,26 @@ Created on Wed Dec  2 14:34:22 2020
 @author: parva
 """
 
-#Reading the test file having the data of footballers and target being their overall skill score ( 1-100 ).
-
+#Reading the test file having the data of footballers and target being their
+#overall skill score ( 1-100 ).
 import pandas as pd
+player_df = pd.read_csv("datascientist/feature_selection/test/CSV/data.csv")
 
-player_df = pd.read_csv("datascientist/feature_selection/test/data.csv")
-
-#Taking only those columns which have numerical or categorical values since feature selection with Pearson Correlation can be performed on numerical data.
-
-numcols = ['Overall', 'Crossing','Finishing',  'ShortPassing',  'Dribbling','LongPassing', 'BallControl', 'Acceleration','SprintSpeed', 'Agility',  'Stamina','Volleys','FKAccuracy','Reactions','Balance','ShotPower','Strength','LongShots','Aggression','Interceptions']
+#Taking only those columns which have numerical or categorical values since 
+#feature selection with Pearson Correlation can be performed on numerical data.
+numcols = ['Overall', 'Crossing','Finishing',  'ShortPassing',  'Dribbling',
+           'LongPassing', 'BallControl', 'Acceleration','SprintSpeed',
+           'Agility',  'Stamina','Volleys','FKAccuracy','Reactions','Balance',
+           'ShotPower','Strength','LongShots','Aggression','Interceptions']
 catcols = ['Preferred Foot','Position','Body Type','Nationality','Weak Foot']
-
 player_df = player_df[numcols+catcols]
 
 #encoding categorical values with one-hot encoding.
-
 traindf = pd.concat([player_df[numcols], pd.get_dummies(player_df[catcols])],axis=1)
 features = traindf.columns
 
+#dropping rows with Nan values
 traindf = traindf.dropna()
-
-
 traindf = pd.DataFrame(traindf,columns=features)
 
 #Separating features(X) and target(y).
@@ -35,39 +34,22 @@ X = X.drop(['Overall'],axis = 1)
 
 
 from datascientist.feature_selection.filter_based_selection import PearsonCorrelation
-    
-
 Col_sel = PearsonCorrelation(X, y)
 
 #using corr_score method with different parameter values.
-
 score1 = Col_sel.corr_score()
-
 score2 = Col_sel.corr_score(sort = True)
-
 score3 = Col_sel.corr_score(reset_index = True)
-
 score4 = Col_sel.corr_score(sort = True,reset_index = True)
 
-
-
 #using top_corr_featurenames method with different parameter values.
-
 topfeatname1 = Col_sel.top_corr_featurenames()
-
 topfeatname2 = Col_sel.top_corr_featurenames(feat_num = 15)
-
 topfeatname3 = Col_sel.top_corr_featurenames(feat_num = 15,ascending = False)
-
 topfeatname4 = Col_sel.top_corr_featurenames(feat_num = 30,ascending = False)
 
 #using top_corr_features method with different parameter values.
-
 X_mod1 = Col_sel.top_corr_features()
-
 X_mod2 = Col_sel.top_corr_features(feat_num = 15)
-
 X_mod3 = Col_sel.top_corr_features(feat_num = 15,ascending = False)
-
 X_mod4 = Col_sel.top_corr_features(feat_num = 30,ascending = False)
-
